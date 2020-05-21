3c12df3e7443f10d72a138149cd6df61e6f3bea6
Bebe29 <bevinda.je@gmail.com>
2020-05-22 00:28:10 +0700

    not found update and footer

---
+++ src/views/screens/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./Footer.css";
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="footer-container">
+        <div className="row">
+          <h1>Footer</h1>
+        </div>
+        <div className="row">©</div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
--- src/views/screens/PageNotFound/PageNotFound.jsx
+++ src/views/screens/PageNotFound/PageNotFound.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
+import "./PageNotFound.css";
+import NotFound from "../../../assets/NotFound/fish.png";
 
 class PageNotFound extends Component {
   render() {
     return (
-      <div>
-        <h1>404</h1>
-        <h2>Sorry, Page not found</h2>
+      <div className="row">
+        <div className="col">
+          <img src={NotFound} alt="" className="notfound-img" />
+        </div>
+        <div className="col">
+          <h1>404</h1>
+          <h2>Sorry, Page not found</h2>
+        </div>
       </div>
     );
   }
