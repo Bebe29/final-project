24fa0a121807e23af475b45e7d9810742ba50c2a
Bevinda Juliani Enge <BE40202X@cimbniaga.co.id>
2020-05-31 19:52:02 +0700

    carousel, json update

--- src/views/screens/Home/Home.jsx
+++ src/views/screens/Home/Home.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import "./Home.css";
-import A from "../../../assets/Carousel/1.jpg";
+// import A from "../../../assets/Carousel/1.jpg";
 import B from "../../../assets/Carousel/2.jpg";
 import C from "../../../assets/Carousel/3.jpg";
 import {
   Carousel,
   CarouselItem,
-  CarouselCaption,
+  // CarouselCaption,
   CarouselIndicators,
   CarouselControl,
 } from "reactstrap";
@@ -50,18 +50,35 @@ class Home extends Component {
           onExited={() => this.setState({ animating: false })}
           key={`carousel_${id}`}
         >
-          <div className="d-flex carousel-container">
+          <div className="row carousel-container">
+            <div
+              className="col custom-carousel-caption"
+              style={{
+                background: `url("${image}")`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                // opacity: "60%",
+                opacity: "75%",
+              }}
+            >
+              <div className="text-center">
+                <h3>{caption}</h3>
+                <h3 className="subtitle-lg">{content}</h3>
+              </div>
+              {/* <CarouselCaption
+                captionHeader={caption}
+                captionText={content}
+                className="carousel-caption"
+              /> */}
+            </div>
+          </div>
+          {/* <div className="d-flex carousel-container">
             <img src={image} alt="" />
             <div className="carousel-caption">
               <h3>{caption}</h3>
               <h3 className="subtitle-lg">{content}</h3>
             </div>
-          </div>
-          {/* <CarouselCaption
-            captionHeader={caption}
-            captionText={content}
-            className="carousel-caption"
-          /> */}
+          </div> */}
         </CarouselItem>
       );
     });
@@ -92,50 +109,49 @@ class Home extends Component {
 
   render() {
     return (
-      // <div>
-      //   <h1>Hello</h1>
-      // </div>
-      <>
-        <Carousel
-          activeIndex={this.state.activeIndex}
-          next={this.next}
-          previous={this.previous}
-        >
-          <CarouselIndicators
-            items={dummyCarousel}
+      <div className="row">
+        <div className="col">
+          <Carousel
             activeIndex={this.state.activeIndex}
-            onClickHandler={this.goToIndex}
-            className="carousel-indicators-round"
-          />
-          {this.renderCarousel()}
-          <CarouselControl
-            direction="prev"
-            directionText="Previous"
-            onClickHandler={this.previous}
-          />
-          <CarouselControl
-            direction="next"
-            directionText="Next"
-            onClickHandler={this.next}
-          />
-        </Carousel>
-        <h1>Hello</h1>
-        <h2>Hello</h2>
-        <h3>Hello</h3>
-        <h4>Hello</h4>
-        <h5>Hello</h5>
-        <h6>Hello</h6>
-        <p>-------</p>
-        <div className="subtitle-lg">Hello</div>
-        <div className="subtitle-md">Hello</div>
-        <div className="subtitle-sm">Hello</div>
-        <div className="subtitle-xs">Hello</div>
-        <div className="content-lg">Hello</div>
-        <div className="content-md">Hello</div>
-        <div className="content-sm">Hello</div>
-        <div className="content-xs">Hello</div>
-        <caption>Hello</caption>
-      </>
+            next={this.next}
+            previous={this.previous}
+          >
+            <CarouselIndicators
+              items={dummyCarousel}
+              activeIndex={this.state.activeIndex}
+              onClickHandler={this.goToIndex}
+              className="carousel-indicators-round"
+            />
+            {this.renderCarousel()}
+            <CarouselControl
+              direction="prev"
+              directionText="Previous"
+              onClickHandler={this.previous}
+            />
+            <CarouselControl
+              direction="next"
+              directionText="Next"
+              onClickHandler={this.next}
+            />
+          </Carousel>
+          <h1>Hello</h1>
+          <h2>Hello</h2>
+          <h3>Hello</h3>
+          <h4>Hello</h4>
+          <h5>Hello</h5>
+          <h6>Hello</h6>
+          <p>-------</p>
+          <div className="subtitle-lg">Hello</div>
+          <div className="subtitle-md">Hello</div>
+          <div className="subtitle-sm">Hello</div>
+          <div className="subtitle-xs">Hello</div>
+          <div className="content-lg">Hello</div>
+          <div className="content-md">Hello</div>
+          <div className="content-sm">Hello</div>
+          <div className="content-xs">Hello</div>
+          <caption>Hello</caption>
+        </div>
+      </div>
     );
   }
 }
