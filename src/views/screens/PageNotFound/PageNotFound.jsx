887da9b241a20dd0157408a63c4641553f5ad2de
Bevinda Juliani Enge <BE40202X@cimbniaga.co.id>
2020-06-20 15:00:48 +0700

    const page not found

--- src/views/screens/PageNotFound/PageNotFound.jsx
+++ src/views/screens/PageNotFound/PageNotFound.jsx
@@ -1,21 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import "./PageNotFound.css";
 import NotFound from "../../../assets/NotFound/fish.png";
 
-class PageNotFound extends Component {
-  render() {
-    return (
-      <div className="row">
-        <div className="col">
-          <img src={NotFound} alt="" className="notfound-img" />
-        </div>
-        <div className="col">
-          <h1>404</h1>
-          <h2>Sorry, Page not found</h2>
-        </div>
+const PageNotFound = () => {
+  return (
+    <div className="notfound-container ">
+      <img src={NotFound} alt="" className="notfound-img" />
+      <div className="notfound-text-container">
+        <h1>404</h1>
+        <h2>Sorry, Page not found</h2>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default PageNotFound;
