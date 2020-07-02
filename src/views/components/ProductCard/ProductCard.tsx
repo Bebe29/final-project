d4788d106f6e42a5a0cd3b677568770cd5cde6e4
Bevinda Juliani Enge <BE40202X@cimbniaga.co.id>
2020-07-02 19:53:23 +0700

    shop

---
+++ src/views/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+// // import React, { CSSProperties } from "react";
+// import "./ProductCard.css";
+
+// interface ProductCardData {
+//   id?: number;
+//   productName?: string;
+//   price?: number;
+//   image?: string;
+// }
+
+// type ProductCardProps = {
+//   data?: ProductCardData
+//   className?: string;
+// };
+
+// class ProductCard extends React.Component<ProductCardProps> {
+// // const ProductCard = (property: ProductCardProps) => {
+// //   const { data } = property;
+//   const {id, productName, price, image} = this.props.data
+//   return (
+//     <div className="card-container">
+//       <img src={data?.image} alt="" className="card-image" />
+//       <div>
+//         <p>{data?.productName}</p>
+//         <p>
+//           {new Intl.NumberFormat("id-ID", {
+//             style: "currency",
+//             currency: "IDR",
+//           }).format(1000)}
+//         </p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ProductCard;
--- src/views/screens/Shop/Shop.jsx
+++ src/views/screens/Shop/Shop.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Shop.css";
 import Navbar from "react-bootstrap/Navbar";
+import ProductCard from "../../components/ProductCard/ProductCard";
 
 const Shop = () => {
   return (
@@ -9,40 +10,12 @@ const Shop = () => {
         <Navbar className="shop-navbar-container">
           Search bar and category
         </Navbar>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
-        <h1>Shop</h1>
+        <ProductCard
+          data={{
+            image:
+              "https://i.pinimg.com/originals/f0/65/73/f06573ec6b3c15205f0dce2a645002a0.jpg",
+          }}
+        />
         <h1>Shop</h1>
       </div>
     </div>
